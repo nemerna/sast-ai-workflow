7ebc2c5ccc3d8734bcd43dae1eabfb303495f935
Itay Krispin <ikrispin@redhat.com>
2025-03-17 14:16:03 +0200

    fix: delete unused function

--- src/Utils/system_utils.py
+++ src/Utils/system_utils.py
@@ -8,13 +8,3 @@ def get_device():
         return "mps" if torch.backends.mps.is_available() else "cpu"
 
     return "cuda" if torch.cuda.is_available() else "cpu"
-
-def extract_file_path(input_str):
-    # Using regex to extract only the file path
-    match = re.search(r"(src/[^:]+):(\d+):(\d+):", input_str)
-
-    if match:
-        file_path = match.group(1)
-        return file_path
-    else:
-        print("No match found.")
